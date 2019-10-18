99ec54e79ec86704039ab393a8259b010f3aa330
LOMMELEN <els.lommelen@inbo.be>
2019-10-18 16:15:41 +0200

    remove differences between years from output

--- R/calculate_dendrometry.R
+++ R/calculate_dendrometry.R
@@ -4,8 +4,6 @@
 #' \itemize{
 #'  \item plot and year
 #'  \item plot, tree species and year
-#'  \item plot (comparison between different years)
-#'  \item plot and tree species (comparison between different years)
 #'  \item individual trees: statuses in different years
 #' }
 #'
@@ -18,16 +16,12 @@
 calculate_dendrometry <- function(data_dendro) {
   by_plot_year <- calculate_dendro_plot_year(data_dendro)
   by_plot_species_year <- calculate_dendro_plot_species_year(data_dendro)
-  by_plot <- calculate_dendro_plot(by_plot_year)
-  by_plot_species <- calculate_dendro_plot_species(by_plot_species_year)
   status_tree <- summarise_status(data_dendro)
 
   return(
     list(
       dendro_by_plot_year = by_plot_year,
       dendro_by_plot_species_year = by_plot_species_year,
-      dendro_by_plot = by_plot,
-      dendro_by_plot_species = by_plot_species,
       dendro_status_tree = status_tree
     )
   )
--- R/calculate_regeneration.R
+++ R/calculate_regeneration.R
@@ -3,8 +3,8 @@
 #' This function makes aggregations of tree generation data on the levels of
 #' \itemize{
 #'  \item plot, height class and year
+#'  \item plot and year
 #'  \item plot, height class, tree species and year
-#'  \item plot, height class and tree species (comparison between different years)
 #' }
 #'
 #' @param data_regeneration dataframe on tree regeneration with variables ...
@@ -20,15 +20,12 @@ calculate_regeneration <- function(data_regeneration) {
     calculate_regeneration_plot_year(data_regeneration)
   by_plot_height_species_year <-
     calculate_regeneration_plot_height_species_year(data_regeneration)
-  by_plot_height_species <-
-    calculate_regeneration_plot_height_species(by_plot_height_species_year)
 
   return(
     list(
       regeneration_by_plot_height_year = by_plot_height_year,
       regeneration_by_plot_year = by_plot_year,
-      regeneration_by_plot_height_species_year = by_plot_height_species_year,
-      regeneration_by_plot_height_species = by_plot_height_species
+      regeneration_by_plot_height_species_year = by_plot_height_species_year
     )
   )
 }
