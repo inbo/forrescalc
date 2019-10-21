c950269ac5653e49ddb26acdeb2a2ee9c34e054c
LOMMELEN <els.lommelen@inbo.be>
2019-10-21 17:38:57 +0200

    add function make_table_wide

---
+++ R/make_table_wide.R
@@ -0,0 +1,29 @@
+#' change dataframe from long to wide format
+#'
+#' This function changes a dataframe from long to wide format, e.g. to show data from 2 different periods in different columns.
+#'
+#' @param table_long dataframe with data in long format
+#' @param column_to_repeat name of the column of which the values have to be added to the column headings
+#' @param columns_for_comparison (vector with) name(s) of the column(s) you want to repeat for each value of \code{column_to_repeat}
+#'
+#' @return the dataframe in long format
+#'
+#' @export
+#'
+#' @importFrom tidyr pivot_wider
+#'
+#' @examples
+#' library(tidyverse)
+#' table_long <- read_github(...) %>%
+#'   filter(plot_id < 110) %>%
+#'   select(plot_id, species, tree_id, period, DBH_mm, AliveDead)
+#' table_wide <-
+#'   make_table_wide(table_long, column_to_repeat = "period",
+#'                   columns_for_comparison = c("DBH_mm", "AliveDead"))
+#'
+make_table_wide <- function(table_long, column_to_repeat, columns_for_comparison) {
+  table_wide <- table_long %>%
+    pivot_wider(names_from = column_to_repeat, values_from = columns_for_comparison)
+
+  return(table_wide)
+}
