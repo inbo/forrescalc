e5be5cabf49d10b52103d0bf550e2e02e5697a20
LOMMELEN <els.lommelen@inbo.be>
2019-10-31 18:01:38 +0100

    add calculations on DAS indicator

---
+++ R/calculate_DAS_indicator.R
@@ -0,0 +1,96 @@
+#' @title calculate DAS indicator
+#'
+#' @description
+#' This function does all preparing steps (selection of relevant plots and reserves) and calculations to obtain the DAS-indicator.
+#'
+#' First the relevant forest reserves are selected based on the following criteria:
+#'
+#' @template selection_criteria_for_DAS
+#'
+#' @template calculate_das_indicator_explanation_part2
+#'
+#' @inheritParams calculate_dendrometry
+#'
+#' @return dataframe with results for DAS indicator on forest level (?)
+#'
+#' @export
+#'
+#' @importFrom assertthat assert_that
+#' @importFrom dplyr %>% distinct filter group_by inner_join left_join select summarise ungroup
+#' @importFrom readr read_csv2
+#' @importFrom rlang .data
+#' @importFrom tidyr pivot_wider
+#'
+calculate_DAS_indicator <- function(data_dendro) {
+  #only consider living trees
+  data_dendro <- data_dendro %>%
+    filter(.data$AliveDead == 11)
+
+  assert_that(
+    all(!is.na(data_dendro$DBH_mm)),
+    msg = "Not all records have a value for 'DBH_mm'"
+  )
+  assert_that(
+    all(!is.na(data_dendro$basal_area_alive_m2)),
+    msg = "Not all records have a value for 'basal_area_alive_m2'"
+  )
+
+  #select relevant plots
+  DAS_reserve <- data_dendro %>%
+    select_for_DAS_indicator(grouping_vars = c("ForestReserve", "year", "period")) %>%
+    inner_join(data_dendro, by = c("ForestReserve", "year", "period")) %>%
+    select_for_DAS_indicator(grouping_vars = c("plot_id", "year", "period")) %>%
+    inner_join(
+      data_dendro %>%
+        select(.data$plot_id, .data$ForestReserve) %>%
+        distinct(),
+      by = c("plot_id")
+    ) %>%
+    group_by(.data$ForestReserve, .data$period, .data$year) %>%
+    mutate(
+      n_plots = n()
+    ) %>%
+    ungroup() %>%
+    filter(.data$n_plots >= 12) %>%
+    #add data again to selected plots and calculate difference in basal area proportion
+    inner_join(data_dendro, by = c("ForestReserve", "plot_id", "year", "period")) %>%
+    left_join(
+      read_delim(
+        system.file("extdata/DAS_tree_groups.csv", package = "forrescalc"),
+        delim = ";", col_types = "cd"
+      ),
+      by = "species"
+    ) %>%
+    group_by(.data$ForestReserve, .data$period, .data$year, .data$group, .data$plot_id) %>%
+    summarise(
+      basal_area_m2_ha = sum(.data$basal_area_alive_m2_ha)
+    ) %>%
+    ungroup() %>%
+    group_by(.data$ForestReserve, .data$period, .data$year, .data$group) %>%
+    summarise(
+      basal_area_m2_ha = mean(.data$basal_area_m2_ha)
+    ) %>%
+    ungroup() %>%
+    group_by(.data$ForestReserve, .data$period, .data$year) %>%
+    mutate(
+      basal_area_proportion =
+        .data$basal_area_m2_ha / sum(.data$basal_area_m2_ha)
+    ) %>%
+    ungroup() %>%
+    pivot_wider(
+      names_from = .data$period,
+      values_from = c("year", "basal_area_m2_ha", "basal_area_proportion")
+    ) %>%
+    group_by(.data$ForestReserve) %>%
+    mutate(
+      d_res =
+        (sum(.data$basal_area_m2_ha_2) / sum(.data$basal_area_m2_ha_1)) ^
+           (1 / (.data$year_2 - .data$year_1)) - 1,
+      d_group =
+        (.data$basal_area_proportion_2 / .data$basal_area_proportion_1) ^
+           (1 / (.data$year_2 - .data$year_1)) - 1
+    ) %>%
+    ungroup()
+
+  return(DAS_reserve)
+}
---
+++ R/select_for_DAS_indicator.R
@@ -0,0 +1,66 @@
+#' @title select relevant plots/reserves to calculate DAS indicator
+#'
+#' @description
+#' This function does the first step(s) of the calculation of the DAS indicator, namely selecting the relevant forest reserves or plots based on the following criteria:
+#'
+#' @template selection_criteria_for_DAS
+#'
+#' @param data_to_select dataframe with at least all grouping_vars and the variables DBH_mm, species, basal_area_alive_m2_ha, and as records preferably only living trees (they are not filtered out here but they should not be in here to meet the requirements of the DAS indicator)
+#' @param grouping_vars vector with variables that should be grouped on during the selection steps.
+#'
+#' @return dataframe with the grouping vars in which records are removed that do not meet the above described criteria
+#'
+#' @export
+#'
+#' @importFrom dplyr %>% distinct filter group_by_at inner_join left_join select_at summarise ungroup
+#' @importFrom readr read_delim
+#' @importFrom rlang .data
+#'
+select_for_DAS_indicator <- function(data_to_select, grouping_vars) {
+  selected_groups <- data_to_select  %>%
+    group_by_at(grouping_vars) %>%
+    summarise(
+      DBH_mm_average = mean(.data$DBH_mm)
+    ) %>%
+    ungroup() %>%
+    filter(.data$DBH_mm_average > 200 & .data$DBH_mm_average < 700) %>%
+    select_at(grouping_vars) %>%
+    distinct() %>%
+    inner_join(data_to_select, by = grouping_vars) %>%
+    left_join(
+      read_delim(
+        system.file("extdata/DAS_tree_groups.csv", package = "forrescalc"),
+        delim = ";", col_types = "cd"
+      ),
+      by = "species"
+    ) %>%
+    group_by_at(c(grouping_vars, "group", "plot_id")) %>%
+    summarise(
+      basal_area_m2_ha = sum(.data$basal_area_alive_m2_ha)
+    ) %>%
+    ungroup() %>%
+    group_by_at(c(grouping_vars, "group")) %>%
+    summarise(
+      basal_area_m2_ha = mean(.data$basal_area_m2_ha)
+    ) %>%
+    ungroup() %>%
+    group_by_at(grouping_vars) %>%
+    mutate(
+      basal_area_proportion =
+        .data$basal_area_m2_ha / sum(.data$basal_area_m2_ha)
+    ) %>%
+    ungroup() %>%
+    filter(
+      !is.na(.data$group) | .data$basal_area_proportion <= 0.95,
+      is.na(.data$group) | .data$basal_area_proportion <= 0.98
+    ) %>%
+    group_by_at(grouping_vars) %>%
+    summarise(
+      total_proportion = sum(.data$basal_area_proportion)
+    ) %>%
+    ungroup() %>%
+    filter(.data$total_proportion > 0.9) %>% #by writing == 1, some results are dropped due to rounding
+    select_at(grouping_vars)
+
+  return(selected_groups)
+}
