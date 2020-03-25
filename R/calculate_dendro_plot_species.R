4cfc8db3041f86eb07c7b08dece0a930b0a58a6f
LOMMELEN <els.lommelen@inbo.be>
2020-03-25 13:50:17 +0100

    same for calculate_dendro_plot_species()

--- R/calculate_dendro_plot_species.R
+++ R/calculate_dendro_plot_species.R
@@ -20,19 +20,20 @@
 #'
 #' @export
 #'
-#' @importFrom dplyr %>% bind_rows filter group_by inner_join mutate summarise transmute ungroup
+#' @importFrom dplyr %>% select summarise transmute
+#' @importFrom tidyr pivot_wider
 #' @importFrom rlang .data
 #'
 calculate_dendro_plot_species <- function(by_plot_species_year) {
   #data from long to wide
   by_plot_species <- by_plot_species_year %>%
+    select(-.data$volume_log_m3_ha, -.data$volume_deadwood_m3_ha) %>%
     pivot_wider(
       names_from = "period",
       values_from =
         c(.data$year, .data$number_of_trees_ha,
           .data$basal_area_alive_m2_ha, .data$basal_area_snag_m2_ha,
-          .data$volume_alive_m3_ha, .data$volume_snag_m3_ha,
-          .data$volume_log_m3_ha, .data$volume_deadwood_m3_ha)
+          .data$volume_alive_m3_ha, .data$volume_snag_m3_ha)
     ) %>%
     transmute(  #calculate: make the comparison
       .data$plot_id,
@@ -48,11 +49,7 @@ calculate_dendro_plot_species <- function(by_plot_species_year) {
       volume_alive_m3_ha_diff =
         .data$volume_alive_m3_ha_2 - .data$volume_alive_m3_ha_1,
       volume_snag_m3_ha_diff =
-        .data$volume_snag_m3_ha_2 - .data$volume_snag_m3_ha_1,
-      volume_log_m3_ha_diff =
-        .data$volume_log_m3_ha_2 - .data$volume_log_m3_ha_1,
-      volume_deadwood_m3_ha_diff =
-        .data$volume_deadwood_m3_ha_2 - .data$volume_deadwood_m3_ha_1
+        .data$volume_snag_m3_ha_2 - .data$volume_snag_m3_ha_1
     )
 
   return(by_plot_species)
