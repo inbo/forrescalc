9b21baf4b241f1ed3d6137f02006f8b90cb79788
LOMMELEN <els.lommelen@inbo.be>
2020-03-30 16:14:27 +0200

    change calculations in calculate_diam_plot_year to make them similar to calculate_diam_plot_species_year

--- R/calculate_diam_plot_year.R
+++ R/calculate_diam_plot_year.R
@@ -3,6 +3,7 @@
 #' This function calculates for each plot, year and diamter class some values per hectare: number of trees, basal area, and volume.
 #'
 #' @inheritParams calculate_dendrometry
+#' @inheritParams calculate_diam_plot_species_year
 #'
 #' @return dataframe with columns plot, year, DBHClass_5cm, number_of_trees_ha, basal_area_m2_ha, volume_m3_ha
 #'
@@ -11,7 +12,12 @@
 #' #change path before running
 #' data_dendro <-
 #'   load_data_dendrometry("C:/MDB_BOSRES_selectieEls/FieldMapData_MDB_BOSRES_selectieEls.accdb")
-#' calculate_diam_plot_year(data_dendro)
+#' data_shoots <-
+#'   load_data_shoots("C:/MDB_BOSRES_selectieEls/FieldMapData_MDB_BOSRES_selectieEls.accdb")
+#' data_stems <- compose_stem_data(data_dendro, data_shoots)
+#' data_deadwood <-
+#'   load_data_deadwood("C:/MDB_BOSRES_selectieEls/FieldMapData_MDB_BOSRES_selectieEls.accdb")
+#' calculate_diam_plot_year(data_stems, data_deadwood)
 #' }
 #'
 #' @export
@@ -19,14 +25,38 @@
 #' @importFrom dplyr %>% filter group_by n summarise ungroup
 #' @importFrom rlang .data
 #'
-calculate_diam_plot_year <- function(data_dendro) {
-  by_diam_plot_year <- data_dendro %>%
-    filter(.data$AliveDead == 11) %>%
-    group_by(.data$plot_id, .data$year, .data$period, .data$DBHClass_5cm) %>%
-    summarise(
-      basal_area_alive_m2_ha = sum(.data$basal_area_alive_m2_ha),
-      volume_alive_m3_ha = sum(.data$volume_alive_m3_ha)
+calculate_diam_plot_year <- function(data_stems, data_deadwood) {
+  by_diam_plot_year <- data_stems %>%
+    group_by(
+      .data$plot_id, .data$year, .data$period, .data$DBHClass_5cm,
+      .data$AliveDead
     ) %>%
-    ungroup()
+    summarise(
+      stem_number_ha = round(sum(n() / .data$plotarea_ha)),
+      basal_area_m2_ha = sum(.data$basal_area_m2 / .data$plotarea_ha)
+    ) %>%
+    ungroup() %>%
+    pivot_wider(
+      names_from = "AliveDead",
+      values_from = c("stem_number_ha", "basal_area_m2_ha")
+    ) %>%
+    rename(
+      stem_number_alive_ha = .data$stem_number_ha_11,
+      stem_number_snag_ha = .data$stem_number_ha_12,
+      basal_area_alive_m2_ha = .data$basal_area_m2_ha_11,
+      basal_area_snag_m2_ha = .data$basal_area_m2_ha_12
+    ) %>%
+    left_join(
+      data_deadwood %>%
+        group_by(
+          .data$plot_id, .data$year, .data$period, .data$DBHClass_5cm
+        ) %>%
+        summarise(
+          log_number_ha = round(sum(n() / .data$plotarea_ha)),
+          volume_log_m3_ha = sum(.data$CalcVolume_m3 / .data$plotarea_ha)
+        ) %>%
+        ungroup(),
+      by = c("plot_id", "year", "period")
+    )
   return(by_diam_plot_year)
 }
