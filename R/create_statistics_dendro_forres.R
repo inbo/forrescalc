8d07d5f297d59649943510f43d611d3eb4842765
LOMMELEN <els.lommelen@inbo.be>
2020-04-23 17:29:01 +0200

    update code in function create_statistics_dendro_forres()

--- R/create_statistics_dendro_forres.R
+++ R/create_statistics_dendro_forres.R
@@ -16,37 +16,26 @@
 #'
 #' @export
 #'
-#' @importFrom dplyr %>% group_by inner_join n n_distinct summarise ungroup
+#' @importFrom dplyr %>% group_by summarise ungroup
 #' @importFrom rlang .data
 #'
 create_statistics_dendro_forres <- function(dendro_by_plot) {
   by_forres_year <- dendro_by_plot %>%
-    mutate(
-      species_alive = ifelse(.data$AliveDead == 11, .data$species, NA)
-    ) %>%
-    group_by(.data$plot_id, .data$year, .data$period) %>%
+    group_by(.data$forest_reserve, .data$year, .data$period) %>%
     summarise(
-      number_of_tree_species = n_distinct(.data$species_alive, na.rm = TRUE),
-      number_of_trees_ha =
-        round(sum(.data$AliveDead == 11) / unique(.data$plotarea_ha)),
-      basal_area_alive_m2_ha = sum(.data$basal_area_alive_m2_ha),
-      basal_area_snag_m2_ha = sum(.data$basal_area_snag_m2_ha),
-      volume_alive_m3_ha = sum(.data$volume_alive_m3_ha),
-      volume_snag_m3_ha = sum(.data$volume_snag_m3_ha)
+      number_of_tree_species = mean(.data$number_of_tree_species),
+      number_of_trees_ha = mean(.data$number_of_trees_ha),
+      stem_number_ha = mean(.data$stem_number_ha),
+      basal_area_alive_m2_ha = mean(.data$basal_area_alive_m2_ha),
+      basal_area_snag_m2_ha = mean(.data$basal_area_snag_m2_ha),
+      volume_alive_m3_ha = mean(.data$volume_alive_m3_ha),
+      volume_snag_m3_ha = mean(.data$volume_snag_m3_ha),
+      vol_stem_m3 = mean(.data$vol_stem_m3),
+      volume_log_m3_ha = mean(.data$volume_log_m3_ha),
+      volume_deadwood_m3_ha = mean(.data$volume_deadwood_m3_ha),
+      stems_per_tree = mean(.data$stems_per_tree)
     ) %>%
-    ungroup() %>%
-    left_join(
-      data_deadwood %>%
-        group_by(.data$plot_id, .data$year, .data$period) %>%
-        summarise(
-          volume_log_m3_ha = sum(.data$CalcVolume_m3) / ((pi * .data$rA4 ^ 2)/10000)
-        ) %>%
-        ungroup(),
-      by = c("plot_id", "year", "period")
-    ) %>%
-    mutate(
-      volume_deadwood_m3_ha = .data$volume_snag_m3_ha + .data$volume_log_m3_ha
-    )
+    ungroup()
 
   return(by_forres_year)
 }
