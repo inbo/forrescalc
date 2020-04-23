58422b78260360597ea00ae27d5c4a935f5dae3a
LOMMELEN <els.lommelen@inbo.be>
2020-04-23 17:14:01 +0200

    update names in (examples of) create_statistics_dendro_forres()

--- R/create_statistics_dendro_forres.R
+++ R/create_statistics_dendro_forres.R
@@ -2,16 +2,16 @@
 #'
 #' This function shows data from the git-repository forresdat on the level of plot and year some values per hectare: number of tree species, number of trees, basal area, and volume.
 #'
-#' @param dendro_by_plot_year data on the level of plot and year, e.g. table dendro_by_plot_year in git repository forresdat or the result of function calculate_dendro_plot_year()
+#' @param dendro_by_plot data on the level of plot and year, e.g. table dendro_by_plot in git repository forresdat or the result of function calculate_dendro_plot()
 #'
 #' @return dataframe with columns forres, year, number_of_tree_species, number_of_trees_ha, basal_area_m2_ha, volume_m3_ha
 #'
 #' @examples
 #' \dontrun{
 #' #change path before running
-#' dendro_by_plot_year <-
-#'   read_git(tablename = "dendro_by_plot_year", repo_path = "C:/gitrepo/forresdat")
-#' statisticize_dendro_forres_year(dendro_by_plot_year)
+#' dendro_by_plot <-
+#'   read_git(tablename = "dendro_by_plot", repo_path = "C:/gitrepo/forresdat")
+#' create_statistics_dendro_forres(dendro_by_plot)
 #' }
 #'
 #' @export
@@ -19,8 +19,8 @@
 #' @importFrom dplyr %>% group_by inner_join n n_distinct summarise ungroup
 #' @importFrom rlang .data
 #'
-create_statistics_dendro_forres <- function(dendro_by_plot_year) {
-  by_forres_year <- dendro_by_plot_year %>%
+create_statistics_dendro_forres <- function(dendro_by_plot) {
+  by_forres_year <- dendro_by_plot %>%
     mutate(
       species_alive = ifelse(.data$AliveDead == 11, .data$species, NA)
     ) %>%
