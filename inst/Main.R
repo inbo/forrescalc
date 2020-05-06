9726c5a52d8b2229e444f9f4a17c097f68d83c24
LOMMELEN <els.lommelen@inbo.be>
2020-05-06 14:18:44 +0200

    replace 0 with NA for deadwood of plot_id 11000 (PR #15 & #17)

--- inst/Main.R
+++ inst/Main.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(forrescalc)
 
 path_to_fieldmap <- "C:/R/bosreservatendb/MDB_BOSRES_selectieEls/FieldMapData_MDB_BOSRES_selectieEls.accdb"
@@ -28,7 +29,37 @@ data_stems  <- compose_stem_data(data_dendro, data_shoots)
 
 dendro <- calculate_dendrometry(data_dendro, data_deadwood, data_stems)
 
-# WARNING: if plotid 11000 in data, replace (lying) deadwood results for plot_id = 11000 by "NA"
+# in KV Kersselaerspleyn (plot 11000) no lying deadwood is meausured
+# if plotid 11000 in data, replace (lying) deadwood results for plot_id = 11000 by "NA"
+dendro[["dendro_by_plot"]] <- dendro[["dendro_by_plot"]] %>%
+  mutate(
+    volume_log_m3_ha  =
+      ifelse(plot_id == 11000 & volume_log_m3_ha == 0, NA, volume_log_m3_ha),
+    volume_deadwood_m3_ha  =
+      ifelse(plot_id == 11000 & is.na(volume_log_m3_ha), NA, volume_log_m3_ha)
+  )
+dendro[["dendro_by_plot_species"]] <- dendro[["dendro_by_plot_species"]] %>%
+  mutate(
+    volume_log_m3_ha  =
+      ifelse(plot_id == 11000 & volume_log_m3_ha == 0, NA, volume_log_m3_ha),
+    volume_deadwood_m3_ha  =
+      ifelse(plot_id == 11000 & is.na(volume_log_m3_ha), NA, volume_log_m3_ha)
+  )
+dendro[["dendro_by_diam_plot"]] <- dendro[["dendro_by_diam_plot"]] %>%
+  mutate(
+    log_number_ha  =
+      ifelse(plot_id == 11000 & log_number_ha == 0, NA, log_number_ha),
+    volume_log_m3_ha  =
+      ifelse(plot_id == 11000 & volume_log_m3_ha == 0, NA, volume_log_m3_ha)
+  )
+dendro[["dendro_by_diam_plot_species"]] <- dendro[["dendro_by_diam_plot_species"]] %>%
+  mutate(
+    log_number_ha  =
+      ifelse(plot_id == 11000 & log_number_ha == 0, NA, log_number_ha),
+    volume_log_m3_ha  =
+      ifelse(plot_id == 11000 & volume_log_m3_ha == 0, NA, volume_log_m3_ha)
+  )
+
 
 save_results_git(
   results = dendro,
