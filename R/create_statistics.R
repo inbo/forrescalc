a281e17fd85342c701ade08a7e39f37289108b33
LOMMELEN <els.lommelen@inbo.be>
2020-05-08 17:53:21 +0200

    create_statistics: calculate mean & var

--- R/create_statistics.R
+++ R/create_statistics.R
@@ -26,26 +26,16 @@
 #'
 #' @export
 #'
-#' @importFrom dplyr %>% group_by summarise ungroup
+#' @importFrom dplyr %>% group_by_at summarise_at ungroup vars
 #' @importFrom rlang .data
 #'
-create_statistics <- function(dataset) {
+create_statistics <- function(dataset, level = "forest_reserve", variables) {
+  level <- c("year", "period", level)
   statistics <- dataset %>%
-    # group_by(.data$forest_reserve, .data$year, .data$period) %>%
-    # summarise(
-    #   number_of_tree_species = mean(.data$number_of_tree_species),
-    #   number_of_trees_ha = mean(.data$number_of_trees_ha),
-    #   stem_number_ha = mean(.data$stem_number_ha),
-    #   basal_area_alive_m2_ha = mean(.data$basal_area_alive_m2_ha),
-    #   basal_area_snag_m2_ha = mean(.data$basal_area_snag_m2_ha),
-    #   volume_alive_m3_ha = mean(.data$volume_alive_m3_ha),
-    #   volume_snag_m3_ha = mean(.data$volume_snag_m3_ha),
-    #   vol_stem_alive_m3_ha = mean(.data$vol_stem_alive_m3_ha),
-    #   vol_stem_snag_m3_ha = mean(.data$vol_stem_snag_m3_ha),
-    #   volume_log_m3_ha = mean(.data$volume_log_m3_ha),
-    #   volume_deadwood_m3_ha = mean(.data$volume_deadwood_m3_ha),
-    #   stems_per_tree = mean(.data$stems_per_tree)
-    # ) %>%
+    group_by_at(vars(level)) %>%
+    summarise_at(
+      variables, list(n = n, mean = mean, var = var)
+    ) %>%
     ungroup()
 
   return(statistics)
