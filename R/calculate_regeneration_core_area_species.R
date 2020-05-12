066ecdbcc08d15f01af4b12d15809245c2cdb045
LOMMELEN <els.lommelen@inbo.be>
2020-05-12 12:22:41 +0200

    update documentation calculate_generation_core_area_species() and add number_of_trees_ha

--- R/calculate_regeneration_core_area_species.R
+++ R/calculate_regeneration_core_area_species.R
@@ -1,20 +1,20 @@
 #' aggregate regeneration parameters by plot, species and year
 #'
-#' This function calculates for each plot, species and year the percentage of subplots in which the species is regenerating. This calculation is designed for core areas, that consist of different subplots.
+#' This function calculates for each plot, species and year the number of trees per ha, and the number and percentage of subplots in which the species is regenerating. This calculation is designed for core areas, that consist of different subplots.
 #'
 #' @inheritParams calculate_regeneration
 #'
-#' @return dataframe with columns plot, species, year, number_of_subplots_with_regeneration and perc_subplots_with_regeneration
+#' @return dataframe with columns plot, species, year, min_number_of_trees_ha, max_number_of_trees_ha, number_of_subplots_with_regeneration and perc_subplots_with_regeneration
 #'
 #' @examples
 #' \dontrun{
 #' #change path before running
 #' data_regeneration_CA <-
-#'   load_data_vegetation(
+#'   load_data_regeneration(
 #'     "C:/MDB_BOSRES_selectieEls/FieldMapData_MDB_BOSRES_selectieEls.accdb",
-#'     plottype == 30
+#'     plottype = "Core area"
 #'   )
-#' calculate_regeneration_plot_species(data_regeneration_CA)
+#' calculate_regeneration_core_area_species(data_regeneration_CA)
 #' }
 #'
 #' @export
@@ -29,6 +29,8 @@ calculate_regeneration_core_area_species <- function(data_regeneration) {
       .data$species
     ) %>%
     summarise(
+      min_number_of_trees_ha = sum(.data$min_number_of_trees / .data$plotarea_ha),
+      max_number_of_trees_ha = sum(.data$max_number_of_trees / .data$plotarea_ha),
       number_of_subplots_with_regeneration = n_distinct(.data$subplot_id),
       perc_subplots_with_regeneration =
         .data$number_of_subplots_with_regeneration * 100 / 98
