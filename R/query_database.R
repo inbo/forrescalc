f4c490583d29c8b01315d59a1c644b6943e26cd8
ElsLommelen <els.lommelen@inbo.be>
2021-04-28 18:52:44 +0200

    fix documentation query_database()

--- R/query_database.R
+++ R/query_database.R
@@ -1,19 +1,21 @@
-#' query all periods from fieldmap database
+#' @title query all periods from fieldmap database
 #'
+#' @description
 #' This helper function retrieves data of all periods from the given database
 #' by using the given query and adding in the period dependent parameters.
 #' Reason for this function is to avoid repetition in the load functions.
 #'
-#' @param query query that is adapted to be used in different periods, in which
-#' following parameters can be used:
-#' - `%1$d` : replaced by the period number,
-#' - `%2$s` : replaced by an empty string in period 1 and by '_?eSet' in all other periods
-#' - `%3$s` : replaced by the string given in argument selection
-#' - `%4$s` : replaced by the string given in argument add_fields
-#' - `%5$s` : replaced by the string given in argument conjunction
-#' @param selection string that will be added to `%3$s` in the query
-#' @param add_fields string that will be added to `%4$s` in the query
-#' @param conjunction string that will be added to `%5$s` in the query
+#' @param query query that is adapted to be used in different periods,
+#' in which the following parameters can be used: \itemize{
+#'   \item `\%1$d` : replaced by the period number,
+#'   \item `\%2$s` : replaced by an empty string in period 1 and by '_?eSet' in all other periods (with ? the period)
+#'   \item `\%3$s` : replaced by the string given in argument selection
+#'   \item `\%4$s` : replaced by the string given in argument add_fields
+#'   \item `\%5$s` : replaced by the string given in argument conjunction
+#' }
+#' @param selection string that will be added to `\%3$s` in the query
+#' @param add_fields string that will be added to `\%4$s` in the query
+#' @param conjunction string that will be added to `\%5$s` in the query
 #' @param n_periods highest period number that is present in the database
 #' (change default here when a new period is added to the database)
 #'
