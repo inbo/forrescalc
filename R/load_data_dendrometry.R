f7bf9c56e2884af03e26565ab2a9b72ec845aeb3
ElsLommelen <els.lommelen@inbo.be>
2021-04-28 14:42:55 +0200

    load_data_dendrometry(): queries samenvoegen

--- R/load_data_dendrometry.R
+++ R/load_data_dendrometry.R
@@ -48,13 +48,12 @@ load_data_dendrometry <-
       ""
     )
   query_dendro <-
-    sprintf(
       "SELECT Plots.ID AS plot_id,
         Plots.Plottype AS plottype,
         IIf(Plots.Area_ha IS NULL, Plots.Area_m2 / 10000, Plots.Area_ha) AS totalplotarea_ha,
         Trees.ID AS tree_measure_id,
         pd.ForestReserve AS forest_reserve,
-        pd.Date_dendro_1eSet AS date_dendro,
+        pd.Date_Dendro_%deSet AS date_dendro,
         pd.rA1 AS r_A1, pd.rA2 AS r_A2, pd.rA3 AS r_A3, pd.rA4 AS r_A4,
         pd.TresHoldDBH_Trees_A3_alive AS dbh_min_a3,
         pd.TresHoldDBH_Trees_A3_dead AS dbh_min_a3_dead,
@@ -76,55 +75,17 @@ load_data_dendrometry <-
         Trees.BasalArea_m2 AS basal_area_m2,
         Trees.IndShtCop AS ind_sht_cop,
         Trees.TreeNumber AS tree_number,
-        Trees.Individual AS individual %s
-      FROM ((Plots INNER JOIN Trees ON Plots.ID = Trees.IDPlots)
-        INNER JOIN PlotDetails_1eSet pd ON Plots.ID = pd.IDPlots) %s;",
-      add_fields, selection
-    )
-
-  query_dendro2 <-
-    sprintf(
-      "SELECT Plots.ID AS plot_id,
-        Plots.Plottype AS plottype,
-        IIf(Plots.Area_ha IS NULL, Plots.Area_m2 / 10000, Plots.Area_ha) AS totalplotarea_ha,
-        Trees.ID AS tree_measure_id,
-        pd.ForestReserve AS forest_reserve,
-        pd.Date_dendro_2eSet AS date_dendro,
-        pd.rA1 AS r_A1, pd.rA2 AS r_A2, pd.rA3 AS r_A3, pd.rA4 AS r_A4,
-        pd.TresHoldDBH_Trees_A3_alive AS dbh_min_a3,
-        pd.TresHoldDBH_Trees_A3_dead AS dbh_min_a3_dead,
-        pd.TresHoldDBH_Trees_A4_alive AS dbh_min_a4,
-        pd.TresHoldDBH_Trees_A4_dead AS dbh_min_a4_dead,
-        pd.TresHoldDBH_Trees_CoreArea_alive AS dbh_min_core_area,
-        pd.TresHoldDBH_Trees_CoreArea_dead AS dbh_min_core_area_dead,
-        pd.LengthCoreArea_m AS length_core_area_m,
-        pd.WidthCoreArea_m AS width_core_area_m,
-        pd.Area_ha AS core_area_ha,
-        Trees.DBH_mm AS dbh_mm,
-        Trees.Height_m AS height_m,
-        Trees.Species AS species,
-        Trees.AliveDead AS alive_dead,
-        Trees.DecayStage AS decaystage,
-        Trees.Vol_tot_m3 AS vol_tot_m3,
-        Trees.Vol_stem_m3 AS vol_stem_m3,
-        Trees.Vol_crown_m3 AS vol_crown_m3,
-        Trees.BasalArea_m2 AS basal_area_m2,
-        Trees.IndShtCop AS ind_sht_cop,
-        Trees.TreeNumber AS tree_number,
-        Trees.Individual AS individual %s,
-        Trees.OldID as old_id
-      FROM ((Plots INNER JOIN Trees_2eSET Trees ON Plots.ID = Trees.IDPlots)
-        INNER JOIN PlotDetails_2eSet pd ON Plots.ID = pd.IDPlots) %s;",
-      add_fields, selection
-    )
+        Trees.Individual AS individual %3$s
+      FROM ((Plots INNER JOIN %2$sTrees ON Plots.ID = Trees.IDPlots)
+        INNER JOIN PlotDetails_%1$deSet pd ON Plots.ID = pd.IDPlots) %4$s;"
 
   con <- odbcConnectAccess2007(database)
-  data_dendro <- sqlQuery(con, query_dendro, stringsAsFactors = FALSE) %>%
+  data_dendro <- sqlQuery(con, sprintf(query_dendro, 1, "", add_fields, selection), stringsAsFactors = FALSE) %>%
     mutate(
       period = 1
     ) %>%
     bind_rows(
-      sqlQuery(con, query_dendro2, stringsAsFactors = FALSE) %>%
+      sqlQuery(con, sprintf(query_dendro, 2, "Trees_2eSET ", add_fields, selection), stringsAsFactors = FALSE) %>%
         mutate(
           period = 2
         )
