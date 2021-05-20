593fcd4e1a45c8ddc066d306d4d679d4faa08ec4
LEYMAN <anja.leyman@inbo.be>
2021-05-20 16:06:51 +0200

    calc_variables_tree_level: volumeberekeningen (en dus ook de hoogteberekeningen) op niveau van stems

--- R/calc_variables_tree_level.R
+++ R/calc_variables_tree_level.R
@@ -43,25 +43,17 @@
 #'
 calc_variables_tree_level <-
   function(data_dendro, data_stems, height_model) {
-  data_dendro1 <- data_dendro %>%
+
+  data_stems1 <- data_stems %>%
     left_join(
-      data_stems %>%
-        group_by(.data$plot_id, .data$tree_measure_id, .data$period) %>%
-        summarise(tree_number = n()) %>%
-        ungroup(),
-      by = c("plot_id", "tree_measure_id", "period")
-    ) %>%
-    mutate(
-      individual = (.data$ind_sht_cop == 10 | .data$ind_sht_cop == 12)
-    ) %>%
-    left_join(
-      height_model,
-      by = c("species", "forest_reserve", "period", "plottype")
+    height_model,
+    by = c("species", "forest_reserve", "period", "plottype")
     )
-  data_dendro2 <- data_dendro1 %>%
+
+  data_stems2 <- data_stems1 %>%
     filter(!is.na(.data$model)) %>%
     bind_rows(
-      data_dendro1 %>%
+      data_stems1 %>%
         filter(is.na(.data$model)) %>%
         select(-.data$model, -.data$P1, -.data$P2) %>%
         left_join(
@@ -81,60 +73,48 @@ calc_variables_tree_level <-
       calc_height_m =
         ifelse(is.na(.data$calc_height_r), .data$calc_height_m, .data$calc_height_r)
     ) %>%
-    select(
-      -.data$dbh_mm, -.data$vol_tot_m3, -.data$vol_stem_m3, -.data$vol_crown_m3
-    ) %>%
+    select(-.data$model, -.data$P1, -.data$P2) %>%
+    # bole volume 1 entry
     left_join(
-      data_stems %>%
-        left_join(
-          suppressMessages(
-            read_csv2(
-              system.file("extdata/tariffs1entry.csv", package = "forrescalc")
-            )
-          ) %>%
-            select(-.data$name_nl, -.data$tariff_id, -.data$tariff_group, -.data$source),
-          by = "species"
-        ) %>%
-        mutate(
-          perimeter = pi * .data$dbh_mm / 10,
-          vol_stem_t1_m3 =
-            .data$a + .data$b * .data$perimeter + .data$c * .data$perimeter ^ 2 +
-            .data$d * .data$perimeter ^ 3
-        ) %>%
-        select(
-          -.data$a, -.data$b, -.data$c, -.data$d
-        ) %>%
-        left_join(
-          suppressMessages(
-            read_csv2(
-              system.file("extdata/tariffs1entry_crown.csv", package = "forrescalc")
-            )
-          ) %>%
-            select(-.data$name_nl, -.data$tariff_id, -.data$tariff_group, -.data$source),
-          by = "species"
-        ) %>%
-        mutate(
-          vol_crown_m3 =
-            .data$a + .data$b * .data$perimeter + .data$c * .data$perimeter ^ 2 +
-            .data$d * .data$perimeter ^ 3,
-          vol_crown_m3 = pmax(0, .data$vol_crown_m3)
-        ) %>%
-        group_by(.data$plot_id, .data$tree_measure_id, .data$period) %>%
-        summarise(
-          decaystage =
-            round(
-              sum(.data$decaystage * .data$dbh_mm ^ 2 / 4) /
-              sum(.data$dbh_mm ^ 2 / 4)
-              ),
-          dbh_mm = sqrt(sum(.data$dbh_mm ^ 2) / n()),
-          basal_area_m2 = sum(.data$basal_area_m2),
-          vol_stem_t1_m3 = sum(.data$vol_stem_t1_m3),
-          vol_crown_m3 = sum(.data$vol_crown_m3),
-          perimeter = mean(.data$perimeter)
-        ) %>%
-        ungroup(),
-      by = c("plot_id", "tree_measure_id", "period")
-    )  %>%
+      suppressMessages(
+        read_csv2(
+          system.file("extdata/tariffs1entry.csv", package = "forrescalc")
+        )
+      ) %>%
+        select(-.data$name_nl, -.data$tariff_id, -.data$tariff_group, -.data$source),
+      by = "species"
+    ) %>%
+    mutate(
+      perimeter = pi * .data$dbh_mm / 10
+      , vol_stem_t1_m3 =
+        .data$a + .data$b * .data$perimeter + .data$c * .data$perimeter ^ 2 +
+        .data$d * .data$perimeter ^ 3
+      , vol_stem_t1_m3 = pmax(0, .data$vol_stem_t1_m3)
+    ) %>%
+    select(
+      -.data$a, -.data$b, -.data$c, -.data$d
+    ) %>%
+    # crown volume 1 entry on shoot level
+    left_join(
+      suppressMessages(
+        read_csv2(
+          system.file("extdata/tariffs1entry_crown.csv", package = "forrescalc")
+        )
+      ) %>%
+        select(-.data$name_nl, -.data$tariff_id, -.data$tariff_group, -.data$source),
+      by = "species"
+    ) %>%
+    mutate(
+      vol_crown_m3 =
+        .data$a + .data$b * .data$perimeter + .data$c * .data$perimeter ^ 2 +
+        .data$d * .data$perimeter ^ 3,
+      vol_crown_m3 = pmax(0, .data$vol_crown_m3)
+    ) %>%
+    select(
+      -.data$a, -.data$b, -.data$c, -.data$d
+    ) %>%
+    # bole volume 2 entries on shoot level
+    # !! (when DH-model or calc_height_m is available)
     left_join(
       suppressMessages(
         read_csv2(
@@ -144,52 +124,79 @@ calc_variables_tree_level <-
         select(-.data$name_nl, -.data$tariff_id, -.data$tariff_group, -.data$source),
       by = "species"
     ) %>%
-    mutate(
-      d_cm = .data$dbh_mm / 10,
-      vol_stem_t2_m3 =
-        ifelse(
-          .data$formula == 1,
-          .data$a + .data$b * .data$perimeter + .data$c * .data$perimeter ^ 2 +
-            .data$d * .data$perimeter ^ 3 + .data$e * .data$calc_height_m +
-            .data$f * .data$calc_height_m * .data$perimeter +
-            .data$g * .data$calc_height_m * .data$perimeter ^ 2,
-          1 / 1000 *
-            #spil
-            (exp(1.10597 * log(.data$calc_height_m) + 1.78865 * log(.data$d_cm) - 3.07192) -
-               #Verlies
-               exp(-4.608923 * log(.data$d_cm) + 3.005989 * log(.data$calc_height_m) -
-                     1.3209 * log(.data$calc_height_m) * log(.data$calc_height_m) +
-                     1.605266 * log(.data$d_cm) * log(.data$calc_height_m) + 5.410272))
-        ),
-      vol_stem_m3 =
-        ifelse(
-          .data$ind_sht_cop == 12 | is.na(.data$vol_stem_t2_m3),
-          .data$vol_stem_t1_m3,
-          .data$vol_stem_t2_m3
-        )
-    ) %>%
+      mutate(
+        d_cm = .data$dbh_mm / 10
+        , vol_stem_t2_m3 =
+          ifelse(
+            .data$formula == 1,
+            yes =
+              .data$a + .data$b * .data$perimeter + .data$c * .data$perimeter ^ 2 +
+              .data$d * .data$perimeter ^ 3 + .data$e * .data$calc_height_m +
+              .data$f * .data$calc_height_m * .data$perimeter +
+              .data$g * .data$calc_height_m * .data$perimeter ^ 2,
+            no =
+              1 / 1000 *
+              #spil
+              (exp(1.10597 * log(.data$calc_height_m) + 1.78865 * log(.data$d_cm) - 3.07192) -
+                 #Verlies
+                 exp(-4.608923 * log(.data$d_cm) + 3.005989 * log(.data$calc_height_m) -
+                       1.3209 * log(.data$calc_height_m) * log(.data$calc_height_m) +
+                       1.605266 * log(.data$d_cm) * log(.data$calc_height_m) + 5.410272))
+          )
+        , vol_stem_t2_m3 = pmax(0, .data$vol_stem_t2_m3)
+        , vol_stem_m3 =
+          ifelse(
+            .data$ind_sht_cop == 12 & is.na(.data$vol_stem_t2_m3),
+            .data$vol_stem_t1_m3,
+            .data$vol_stem_t2_m3
+          )
+      ) %>%
     select(
       -.data$a, -.data$b, -.data$c, -.data$d, -.data$e, -.data$f, -.data$g,
-      -.data$formula, -.data$d_cm
-    ) %>%
-    mutate(
-      # volume correction for snags
-      vol_crown_m3 = ifelse(.data$intact_snag == 10, 0, .data$vol_crown_m3),
-      vol_stem_m3 = ifelse(.data$intact_snag == 10, .data$calc_height_m*pi*(.data$dbh_mm/2000)^2, .data$vol_stem_m3),
-      # TIJDELIJK vol_stem_m3 berekend als cilinder cfr. VBI (soms over- en soms onderschatting)
-      # OP TERMIJN functie van Ifer (in afzonderlijke functie te stoppen)
-      # volume correction for broken crown or branches
-      reduction_crown =
-        ifelse(is.na(.data$crown_volume_reduction), 0, .data$crown_volume_reduction),
-      vol_crown_m3 = .data$vol_crown_m3 * (1 - .data$reduction_crown),
-      reduction_branch =
-        ifelse(is.na(.data$branch_length_reduction), 0, .data$branch_length_reduction),
-      vol_crown_m3 = .data$vol_crown_m3 * (1 - .data$reduction_branch),
-      vol_tot_m3 = .data$vol_stem_m3 + .data$vol_crown_m3
-    ) %>%
-    select(
-      -.data$perimeter
+      -.data$formula, -.data$d_cm, -.data$perimeter
     )
 
-  return(data_dendro2)
+  data_dendro1 <- data_dendro %>%
+    select(
+      -.data$dbh_mm, -.data$vol_tot_m3, -.data$vol_stem_m3, -.data$vol_crown_m3
+    ) %>%
+    left_join(
+      data_stems2 %>%
+        group_by(.data$plot_id, .data$tree_measure_id, .data$period) %>%
+        summarise(
+          tree_number = n()
+          , decaystage =
+            round(
+              sum(.data$decaystage * .data$dbh_mm ^ 2 / 4) /
+                sum(.data$dbh_mm ^ 2 / 4)
+            )
+          , dbh_mm = round(sqrt(sum(.data$dbh_mm ^ 2) / n()))
+          , basal_area_m2 = sum(.data$basal_area_m2)
+          , vol_stem_t1_m3 = sum(.data$vol_stem_t1_m3)
+          , vol_stem_t2_m3 = sum(.data$vol_stem_t2_m3)
+          , vol_stem_m3 = sum(.data$vol_stem_m3)
+          , vol_crown_m3 = sum(.data$vol_crown_m3)
+          ) %>%
+        ungroup(),
+      by = c("plot_id", "tree_measure_id", "period")
+    ) %>%
+    mutate(
+      individual = (.data$ind_sht_cop == 10 | .data$ind_sht_cop == 12)
+      # volume correction for snags
+      , vol_crown_m3 = ifelse(.data$intact_snag == 10, 0, .data$vol_crown_m3)
+      , vol_stem_m3 = ifelse(.data$intact_snag == 10, .data$calc_height_m*pi*(.data$dbh_mm/2000)^2, .data$vol_stem_m3)
+            # TIJDELIJK vol_stem_m3 berekend als cilinder cfr. VBI (soms over- en soms onderschatting)
+            # OP TERMIJN functie van Ifer (in afzonderlijke functie te stoppen)
+      # volume correction for broken crown or branches
+      , reduction_crown =
+        ifelse(is.na(.data$crown_volume_reduction), 0, .data$crown_volume_reduction)
+      , vol_crown_m3 = .data$vol_crown_m3 * (1 - .data$reduction_crown)
+      , reduction_branch =
+        ifelse(is.na(.data$branch_length_reduction), 0, .data$branch_length_reduction)
+      , vol_crown_m3 = .data$vol_crown_m3 * (1 - .data$reduction_branch)
+      # total volume
+      , vol_tot_m3 = .data$vol_stem_m3 + .data$vol_crown_m3
+    )
+
+  return(data_dendro1)
 }
