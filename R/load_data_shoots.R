b47f22e2e0043f06d9d76425878c0d36c4869d29
LEYMAN <anja.leyman@inbo.be>
2021-06-01 13:53:29 +0200

    load_data_shoots: - intact_snag op shoot-niveau ook meenemen

--- R/load_data_shoots.R
+++ R/load_data_shoots.R
@@ -22,10 +22,12 @@ load_data_shoots <- function(database) {
       Shoots.ID AS shoot_measure_id,
       Shoots.DBH_mm AS dbh_mm,
       Shoots.Height_m AS height_m,
-      Shoots.DecayStage_Shoots as decaystage
+      Shoots.IntactSnag AS intact_snag,
+      Shoots.DecayStage_Shoots AS decaystage
     FROM Shoots%2$s Shoots;"
 
-  data_shoots <- query_database(database, query_shoots)
+  data_shoots <- query_database(database, query_shoots) %>%
+    mutate(intact_snag = ifelse(is.na(intact_snag), 11, intact_snag))
 
   return(data_shoots)
 }
