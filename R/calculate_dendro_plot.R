832b0b280e4a4bc9e8d6a8553d2f821fd49c6107
ElsLommelen <els.lommelen@inbo.be>
2021-07-29 17:16:32 +0200

    typo's in examples

--- R/calculate_dendro_plot.R
+++ R/calculate_dendro_plot.R
@@ -19,7 +19,7 @@
 #'   load_data_deadwood("C:/MDB_BOSRES_selectieEls/FieldMapData_MDB_BOSRES_selectieEls.accdb")
 #' data_stems <- compose_stem_data(data_dendro, data_shoots)
 #' height_model <- load_height_models("C:/bosreservaten/Hoogtemodellen/")
-#' data_stems_calc <- calc_variables_stem_level(data_stems, heightmodel)
+#' data_stems_calc <- calc_variables_stem_level(data_stems, height_model)
 #' data_dendro_calc <- calc_variables_tree_level(data_dendro, data_stems_calc)
 #' calculate_dendro_plot(data_dendro_calc, data_deadwood)
 #' }
