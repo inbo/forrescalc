b31a05fe22511f159ac5dbac8d9ceaa407f3c079
ElsLommelen <els.lommelen@inbo.be>
2021-08-20 13:55:05 +0200

    load_data_deadwood(): group_by verhuizen naar query om issue #89 op te lossen (fixes #89)

--- R/load_data_deadwood.R
+++ R/load_data_deadwood.R
@@ -58,31 +58,25 @@ load_data_deadwood <-
       Deadwood.DecayStage AS decaystage,
       Deadwood.CalcVolume_m3 AS calc_volume_m3,
       Deadwood.CalcLength_m AS calc_length_m,
-      Deadw_Diam.Distance_m AS length_m,
-      Deadw_Diam.Diameter_mm AS diam_mm %4$s
+      Deadw_Diam.total_length_m,
+      Deadw_Diam.min_diam_mm,
+      Deadw_Diam.max_diam_mm %4$s
     FROM ((((Plots INNER JOIN Deadwood%2$s Deadwood ON Plots.ID = Deadwood.IDPlots)
       INNER JOIN PlotDetails_%1$deSet pd ON Plots.ID = pd.IDPlots)
       INNER JOIN qPlotType ON Plots.Plottype = qPlotType.ID)
-      LEFT JOIN Deadwood%2$s_Diameters Deadw_Diam
+      LEFT JOIN
+        (SELECT IDDeadwood%2$s, IDPlots,
+          SUM(Distance_m) AS total_length_m,
+          MIN(Diameter_mm) AS min_diam_mm,
+          MAX(Diameter_mm) AS max_diam_mm
+        FROM Deadwood%2$s_Diameters
+        GROUP BY IDDeadwood%2$s, IDPlots) Deadw_Diam
         ON Deadwood.ID = Deadw_Diam.IDDeadwood%2$s)
       WHERE Plots.ID = Deadw_Diam.IDPlots %3$s;"
 
   data_deadwood <-
     query_database(database, query_deadwood,
                    selection = selection, add_fields = add_fields) %>%
-    group_by(
-      .data$plot_id, .data$plottype, .data$totalplotarea_ha,
-      .data$forest_reserve, .data$date_dendro, .data$r_A1, .data$r_A2,
-      .data$r_A3, .data$r_A4, .data$length_core_area_m, .data$width_core_area_m,
-      .data$core_area_ha, .data$lying_deadw_id, .data$species, .data$decaystage,
-      .data$calc_volume_m3, .data$calc_length_m, .data$period
-    ) %>%
-    summarise(
-      max_diam_mm = max(.data$diam_mm),
-      min_diam_mm = min(.data$diam_mm),
-      total_length_m = sum(.data$length_m)
-    ) %>%
-    ungroup() %>%
     mutate(
       year = year(round_date(.data$date_dendro, "year")) - 1,
       dbh_class_5cm = give_diamclass_5cm(.data$max_diam_mm),
