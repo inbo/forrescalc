c430e85c54efcf4402fe5e6e8add00092cd3c924
leymanan <anja.leyman@inbo.be>
2022-04-07 13:30:43 +0200

    Main.R: verwijderen code voor niet opgemeten deadwood. Wordt nu in functie load_data_dendro opgevangen.

--- inst/Main.R
+++ inst/Main.R
@@ -8,7 +8,8 @@ path_to_height_models <- "C:/Hoogtemodellen"
 #only when q-tables in Fieldmap have changed (and only mention the changed table)
 from_access_to_git(
   database = path_to_fieldmap,
-  tables = c("qAliveDead", "qSpecies", "qHeightClass_regeneration", "qnumber_regeneration_classes", "qdecaystage"),
+  tables = c("qAliveDead", "qSpecies", "qHeightClass_regeneration",
+             "qnumber_regeneration_classes", "qdecaystage"),
   repo_path = path_to_git_forresdat
 )
 
@@ -33,39 +34,8 @@ data_shoots <-
 data_stems  <- compose_stem_data(data_dendro, data_shoots)
 height_model <- load_height_models(path_to_height_models)
 
-dendro <- calculate_dendrometry(data_dendro, data_deadwood, data_stems, height_model)
-
-# in KV Kersselaerspleyn (plot 11000) no lying deadwood is meausured
-# if plotid 11000 in data, replace (lying) deadwood results for plot_id = 11000 by "NA"
-dendro[["dendro_by_plot"]] <- dendro[["dendro_by_plot"]] %>%
-  mutate(
-    vol_log_m3_ha  =
-      ifelse(plot_id == 11000 & vol_log_m3_ha == 0, NA, vol_log_m3_ha),
-    vol_deadw_m3_ha  =
-      ifelse(plot_id == 11000 & is.na(vol_log_m3_ha), NA, vol_deadw_m3_ha)
-  )
-dendro[["dendro_by_plot_species"]] <- dendro[["dendro_by_plot_species"]] %>%
-  mutate(
-    vol_log_m3_ha  =
-      ifelse(plot_id == 11000 & vol_log_m3_ha == 0, NA, vol_log_m3_ha),
-    vol_deadw_m3_ha  =
-      ifelse(plot_id == 11000 & is.na(vol_log_m3_ha), NA, vol_deadw_m3_ha)
-  )
-dendro[["dendro_by_diam_plot"]] <- dendro[["dendro_by_diam_plot"]] %>%
-  mutate(
-    log_number_ha  =
-      ifelse(plot_id == 11000 & log_number_ha == 0, NA, log_number_ha),
-    vol_log_m3_ha  =
-      ifelse(plot_id == 11000 & vol_log_m3_ha == 0, NA, vol_log_m3_ha)
-  )
-dendro[["dendro_by_diam_plot_species"]] <- dendro[["dendro_by_diam_plot_species"]] %>%
-  mutate(
-    log_number_ha  =
-      ifelse(plot_id == 11000 & log_number_ha == 0, NA, log_number_ha),
-    vol_log_m3_ha  =
-      ifelse(plot_id == 11000 & vol_log_m3_ha == 0, NA, vol_log_m3_ha)
-  )
-
+dendro <- calculate_dendrometry(data_dendro, data_deadwood, data_stems,
+                                height_model)
 
 save_results_git(
   results = dendro,
