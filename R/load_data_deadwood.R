1c608ccd6c6590a0675118aa3a9f272f4260c08d
Anja Leyman <anja.leyman@inbo.be>
2022-09-20 11:50:20 +0200

    load_data_deadwood: - factor 10.000 vergeten

--- R/load_data_deadwood.R
+++ R/load_data_deadwood.R
@@ -90,7 +90,7 @@ load_data_deadwood <-
       plotarea_ha =
         ifelse(
           .data$plottype == "CA",
-          .data$length_core_area_m * .data$width_core_area_m,
+          .data$length_core_area_m * .data$width_core_area_m/10000,
           .data$plotarea_ha
         ),
       plotarea_ha =
