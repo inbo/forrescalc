503dfd23bd93d5440114c924eba4a9458910659d
ElsLommelen <els.lommelen@inbo.be>
2023-11-08 15:43:26 +0100

    add volume standing deadwood to functions calculate_deadw_decay_plot() and calculate_deadw_decay_plot_species() (fixes #45)

--- R/calculate_deadw_decay_plot_species.R
+++ R/calculate_deadw_decay_plot_species.R
@@ -1,7 +1,7 @@
 #' aggregate parameters by decay stage, plot, tree species and year
 #'
 #' This function calculates for each plot, tree species and year the volume
-#' logs per hectare and per decay stage.
+#' logs and standing dead wood per hectare and per decay stage.
 #'
 #' @inheritParams calculate_dendrometry
 #'
@@ -13,7 +13,17 @@
 #' #change path before running
 #' data_deadwood <-
 #'   load_data_deadwood("C:/MDB_BOSRES_selectieEls/FieldMapData_MDB_BOSRES_selectieEls.accdb")
-#' calculate_deadw_decay_plot_species(data_deadwood)
+#' data_dendro <-
+#'   load_data_dendrometry("C:/MDB_BOSRES_selectieEls/FieldMapData_MDB_BOSRES_selectieEls.accdb")
+#' data_shoots <-
+#'   load_data_shoots("C:/MDB_BOSRES_selectieEls/FieldMapData_MDB_BOSRES_selectieEls.accdb")
+#' data_stems <- compose_stem_data(data_dendro, data_shoots)
+#' height_model <- load_height_models("C:/bosreservaten/Hoogtemodellen/")
+#' data_stems_calc <- calc_variables_stem_level(data_stems, height_model)
+#' data_dendro_calc <- calc_variables_tree_level(data_dendro, data_stems_calc)
+#' plotinfo <-
+#'   load_plotinfo("C:/MDB_BOSRES_selectieEls/FieldMapData_MDB_BOSRES_selectieEls.accdb")
+#' calculate_deadw_decay_plot_species(plotinfo, data_deadwood, data_dendro_calc)
 #' }
 #'
 #' @export
@@ -21,9 +31,11 @@
 #' @importFrom dplyr %>% group_by summarise ungroup
 #' @importFrom rlang .data
 #'
-calculate_deadw_decay_plot_species <- function(data_deadwood) {
-  by_decay_plot_species <-
-    data_deadwood %>%
+calculate_deadw_decay_plot_species <-
+  function(plotinfo, data_deadwood = NA, data_dendro_calc = NA) {
+
+  if (is.data.frame(data_deadwood)) {
+    by_decay_plot_species_log <- data_deadwood %>%
       group_by(
         .data$plot_id, .data$year, .data$period, .data$species, .data$decaystage
       ) %>%
@@ -31,6 +43,78 @@ calculate_deadw_decay_plot_species <- function(data_deadwood) {
         vol_log_m3_ha = sum(.data$calc_volume_m3 / .data$plotarea_ha)
       ) %>%
       ungroup()
+  }
+
+  if (is.data.frame(data_dendro_calc)) {
+    by_decay_plot_species_standing <- data_dendro_calc %>%
+      group_by(
+        .data$plot_id, .data$year, .data$period, .data$species, .data$decaystage
+      ) %>%
+      summarise(
+        vol_dead_standing_m3_ha = sum(.data$vol_dead_standing_m3_ha),
+        vol_bole_dead_m3_ha = sum(.data$vol_bole_dead_m3_ha)
+      ) %>%
+      ungroup()
+  }
+
+  if (is.data.frame(data_deadwood)) {
+    if (is.data.frame(data_dendro_calc)) {
+      by_decay_plot_species <- by_decay_plot_species_standing %>%
+        full_join(
+          by_decay_plot_species_log,
+          by = c("plot_id", "year", "period", "species", "decaystage")
+        )
+    } else {
+      by_decay_plot_species <- by_decay_plot_species_log %>%
+        mutate(
+          vol_dead_standing_m3_ha = NA,
+          vol_bole_dead_m3_ha = NA
+        )
+    }
+  } else {
+    if (is.data.frame(data_dendro_calc)) {
+      by_decay_plot_species <- by_decay_plot_species_standing %>%
+        mutate(
+          vol_log_m3_ha = NA
+        )
+    } else {
+      by_decay_plot_species <- NA
+    }
+  }
+
+  if (is.data.frame(by_decay_plot_species)) {
+    by_decay_plot_species <- plotinfo %>%
+      select(
+        "plot_id", year = "year_dendro", "period", "survey_trees",
+        "survey_deadw"
+      ) %>%
+      left_join(
+        by_decay_plot_species, by = c("plot_id", "year", "period")
+      ) %>%
+      filter(.data$survey_trees | .data$survey_deadw) %>%
+      mutate(
+        vol_log_m3_ha =
+          ifelse(
+            is.na(.data$vol_log_m3_ha) & .data$survey_trees,
+            0,
+            .data$vol_log_m3_ha
+          ),
+        vol_dead_standing_m3_ha =
+          ifelse(
+            is.na(.data$vol_dead_standing_m3_ha) & .data$survey_deadw,
+            0,
+            .data$vol_dead_standing_m3_ha
+          ),
+        vol_bole_dead_m3_ha =
+          ifelse(
+            is.na(.data$vol_bole_dead_m3_ha) & .data$survey_deadw,
+            0,
+            .data$vol_bole_dead_m3_ha
+          ),
+        survey_trees = NULL,
+        survey_deadw = NULL
+      )
+  }
 
   return(by_decay_plot_species)
 }
