53dd1ea93cf5585bb765edb87aa94b88529e513c
ElsLommelen <els.lommelen@inbo.be>
2024-01-04 14:33:13 +0100

    update from_access_to_git() to use with sqlite as well (and DBI instead of RODBC)

--- R/from_git_to_access.R
+++ R/from_git_to_access.R
@@ -13,16 +13,20 @@
 #' @export
 #'
 #' @importFrom git2rdata pull repository read_vc
-#' @importFrom RODBC odbcClose odbcConnectAccess2007 sqlDrop sqlSave sqlTables
+#' @importFrom DBI dbDisconnect
 #'
 #' @examples
 #' \dontrun{
 #' #change paths before running
 #' library(forrescalc)
+#' # (add path to your own database here)
+#' path_to_database <-
+#'   system.file("example/database/mdb_bosres.sqlite", package = "forrescalc")
 #' from_git_to_access(
 #'   tables = c("qAliveDead", "qdecaystage"),
 #'   repo_path = "C:/gitrepo/forresdat",
-#'   database = "C:/db/testdb.accdb"
+#'   database = path_to_database,
+#'   remove_tables = TRUE
 #' )
 #' }
 #'
@@ -30,16 +34,41 @@ from_git_to_access <-
   function(tables, repo_path, database, remove_tables = FALSE) {
   repo <- repository(repo_path)
   pull(repo, credentials = get_cred(repo))
-  con <- odbcConnectAccess2007(database)
+  con <- connect_to_database(database)
   for (tablename in tables) {
     dataset <- read_vc(file = paste0("data/", tablename), root = repo)
-    if (remove_tables) {
-      dbtables <- sqlTables(con)
-      if (tablename %in% dbtables$TABLE_NAME) {
-        sqlDrop(con, tablename)
+    tryCatch(
+      dbWriteTable(
+        conn = con, name = tablename, value = dataset, overwrite = remove_tables
+      ),
+      error = function(e) {
+        val <- withCallingHandlers(e)
+        if (
+          endsWith(
+            val[["message"]],
+            "exists in database, and both overwrite and append are FALSE"
+          )
+        ) {
+          tablename <-
+            sub(
+              "Error\\: Table","", val[["message"]]
+            )
+          tablename <-
+            sub(
+              "exists in database, and both overwrite and append are FALSE",
+              "", tablename
+            )
+          stop(
+            paste0(
+              tablename,
+              "exists in database, add 'remove_tables = TRUE' to overwrite it"
+            ),
+            call. = FALSE
+          )
+        }
+        stop(e)
       }
-    }
-    sqlSave(con, dataset, tablename = tablename)
+    )
   }
-  odbcClose(con)
+  dbDisconnect(con)
 }
