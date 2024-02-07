bf784e227f59157de9dc1027201c0e0282b3ab07
ElsLommelen <els.lommelen@inbo.be>
2024-02-07 14:05:54 +0100

    add function check_data_plots() (issue #66)

---
+++ R/check_data_plots.R
@@ -0,0 +1,79 @@
+#' check table plots from fieldmap database for inconsistencies
+#'
+#' This function retrieves the important fields of table plots from the given
+#' database and checks for missing data or wrong input.
+#'
+#' @inheritParams check_data_trees
+#'
+#' @return Dataframe with inconsistent data with ID's and additional columns
+#' `aberrant_field` (which column is wrong) and `anomaly` (what is wrong with
+#' the input)
+#'
+#' @examples
+#' library(forrescalc)
+#' # (add path to your own fieldmap database here)
+#' path_to_fieldmapdb <-
+#'   system.file("example/database/mdb_bosres.sqlite", package = "forrescalc")
+#' check_data_plots(path_to_fieldmapdb)
+#'
+#' @export
+#'
+#' @importFrom DBI dbDisconnect dbGetQuery
+#' @importFrom rlang .data
+#' @importFrom dplyr %>% group_by mutate summarise ungroup
+#' @importFrom tidyr pivot_longer
+#'
+check_data_plots <- function(database) {
+  query_plots <-
+    "SELECT ID As plot_id,
+      Plottype AS plottype_id,
+      Homogeneous AS homogeneous_id
+    FROM Plots;"
+
+  query_plottype <-
+    "SELECT ID as plottype_id
+    FROM qPlotType;"
+
+  con <- connect_to_database(database)
+  data_plots <- dbGetQuery(con, query_plots)
+  data_plottype <- dbGetQuery(con, query_plottype)
+  dbDisconnect(con)
+
+  incorrect_plots <- data_plots %>%
+    mutate(
+      field_plottype = ifelse(is.na(.data$plottype_id), "missing", NA),
+      field_plottype =
+        ifelse(
+          !is.na(.data$plottype_id) &
+            !.data$plottype_id %in% data_plottype$plottype_id,
+          "not in lookuplist",
+          .data$field_plottype
+        ),
+      field_homogeneous = ifelse(is.na(.data$homogeneous_id), "missing", NA),
+      field_homogeneous =
+        ifelse(
+          !is.na(.data$homogeneous_id) & !.data$homogeneous_id %in% c(10, 20),
+          "not in lookuplist",
+          .data$field_homogeneous
+        )
+    ) %>%
+    pivot_longer(
+      cols = c(starts_with("field_")),
+      names_to = "aberrant_field",
+      values_to = "anomaly",
+      values_drop_na = TRUE
+    ) %>%
+    mutate(
+      aberrant_field = gsub("^field_", "", .data$aberrant_field)
+    ) %>%
+    group_by(
+      .data$plot_id
+    ) %>%
+    summarise(
+      aberrant_field = paste0(.data$aberrant_field, collapse = " / "),
+      anomaly = paste0(.data$anomaly, collapse = " / ")
+    ) %>%
+    ungroup()
+
+  return(incorrect_plots)
+}
