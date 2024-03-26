5fecbbb2bd958ad0364d2cc9b416bdf778ec65b5
ElsLommelen <els.lommelen@inbo.be>
2024-03-26 15:49:38 +0100

    from_git_to_access(): use read_forresdat() to read the data (fixes #136)

--- R/from_git_to_access.R
+++ R/from_git_to_access.R
@@ -1,42 +1,50 @@
 #' copy table(s) from git repository to access db
 #'
-#' This function loads one or more tables from  a git repository and saves them
-#' in an access database.
+#' This function loads one or more tables from git repository forresdat
+#' and saves them in an Access (or SQLite) database.
 #'
+#' @inheritParams save_results_access
 #' @inheritParams from_access_to_git
 #' @inheritParams load_data_dendrometry
-#' @inheritParams save_results_git
-#' @inheritParams save_results_access
+#' @inheritParams read_forresdat
 #'
 #' @return No value is returned, the tables are saved in the access database.
 #'
 #' @export
 #'
-#' @importFrom git2rdata pull repository read_vc
-#' @importFrom DBI dbDisconnect
+#' @importFrom DBI dbDisconnect dbWriteTable
 #'
 #' @examples
-#' \dontrun{
-#' #change paths before running
 #' library(forrescalc)
 #' # (add path to your own database here)
-#' path_to_database <-
-#'   system.file("example/database/mdb_bosres.sqlite", package = "forrescalc")
+#' path_to_database <- "my-db.sqlite"
+#' from_git_to_access(
+#'   tables = "dendro_by_plot",
+#'   database = path_to_database
+#' )
+#' # if tables don't contain column plot_id, or it is not relevant to add
+#' # information on the plots, add argument join_plotinfo = FALSE
 #' from_git_to_access(
 #'   tables = c("qAliveDead", "qdecaystage"),
-#'   repo_path = "C:/gitrepo/forresdat",
 #'   database = path_to_database,
-#'   remove_tables = TRUE
+#'   join_plotinfo = FALSE
 #' )
-#' }
+#'
+#' file.remove("my-db.sqlite")
 #'
 from_git_to_access <-
-  function(tables, repo_path, database, remove_tables = FALSE) {
-  repo <- repository(repo_path)
-  pull(repo, credentials = get_cred(repo))
+  function(
+    tables, database, remove_tables = FALSE, plottype = NA, join_plotinfo = TRUE
+  ) {
+  if (is.na(plottype)) {
+    plottype <- "all"
+  }
   con <- connect_to_database(database)
   for (tablename in tables) {
-    dataset <- read_vc(file = paste0("data/", tablename), root = repo)
+    dataset <-
+      read_forresdat(
+        tablename, join_plotinfo = join_plotinfo, plottype = plottype
+      )
     tryCatch(
       dbWriteTable(
         conn = con, name = tablename, value = dataset, overwrite = remove_tables
