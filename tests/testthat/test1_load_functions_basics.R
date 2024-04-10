36e7eaa60fe3e0400cb065a8f2a329cb27b4bae9
ElsLommelen <els.lommelen@inbo.be>
2024-04-10 18:04:26 +0200

    add unittest to check column names/order of load functions (related to issue #68)

---
+++ tests/testthat/test1_load_functions_basics.R
@@ -0,0 +1,133 @@
+context("test load functions basics")
+
+path_to_fieldmapdb <-
+  system.file("example/database/mdb_bosres.sqlite", package = "forrescalc")
+
+describe("load_data_deadwood()", {
+  data_deadwood <- load_data_deadwood(path_to_fieldmapdb)
+  it("output columns are correct", {
+    expect_equal(
+      colnames(data_deadwood),
+      c("plot_id", "plottype", "totalplotarea_ha", "forest_reserve",
+        "date_dendro", "r_A1", "r_A2", "r_A3", "r_A4",
+        "length_core_area_m", "width_core_area_m", "core_area_ha",
+        "lying_deadw_id", "species", "decaystage", "intact_fragm",
+        "calc_volume_m3", "calc_length_m", "total_length_m",
+        "min_diam_mm", "max_diam_mm", "period", "year", "dbh_class_5cm",
+        "plotarea_ha")
+    )
+  })
+})
+
+describe("load_data_dendrometry()", {
+  data_dendro <- load_data_dendrometry(path_to_fieldmapdb)
+  it("output columns are correct", {
+    expect_equal(
+      colnames(data_dendro),
+      c("plot_id", "plottype", "totalplotarea_ha", "tree_measure_id", "old_id",
+        "forest_reserve", "date_dendro", "r_A1", "r_A2", "r_A3", "r_A4",
+        "dbh_min_a3", "dbh_min_a3_dead", "dbh_min_a4", "dbh_min_a4_dead",
+        "dbh_min_core_area", "dbh_min_core_area_dead",
+        "length_core_area_m", "width_core_area_m", "core_area_ha",
+        "dbh_mm", "height_m", "species", "alive_dead", "intact_snag",
+        "decaystage", "calc_height_fm",
+        "crown_volume_reduction", "branch_length_reduction",
+        "ind_sht_cop", "nr_of_stems", "period", "year",
+        "subcircle", "subcirclearea_ha", "plotarea_ha", "dbh_class_5cm")
+    )
+  })
+})
+
+describe("load_data_herblayer()", {
+  data_herblayer <- load_data_herblayer(path_to_fieldmapdb)
+  it("output columns are correct", {
+    expect_equal(
+      colnames(data_herblayer),
+      c("plot_id", "plottype", "totalplotarea_ha", "forest_reserve",
+        "length_core_area_m", "width_core_area_m", "core_area_ha",
+        "subplot_id", "date_vegetation", "species", "coverage_id",
+        "browse_index_id", "period", "year", "plotarea_ha",
+        "coverage_class_average_perc")
+    )
+  })
+})
+
+describe("load_data_regeneration()", {
+  data_regeneration <- load_data_regeneration(path_to_fieldmapdb)
+  it("output columns are correct", {
+    expect_equal(
+      colnames(data_regeneration),
+      c("plot_id", "plottype", "totalplotarea_ha", "forest_reserve",
+         "r_A2","r_A1",
+        "length_core_area_m", "width_core_area_m", "core_area_ha",
+        "subplot_id", "date_regeneration", "height_class", "species",
+        "number_class", "nr_of_regeneration", "rubbing_damage_number",
+        "period", "year", "subcircle", "subcirclearea_ha", "plotarea_ha",
+        "rubbing_damage_perc", "approx_nr_regeneration",
+        "min_number_of_regeneration", "max_number_of_regeneration")
+    )
+  })
+})
+
+describe("load_data_shoots()", {
+  data_shoots <- load_data_shoots(path_to_fieldmapdb)
+  it("output columns are correct", {
+    expect_equal(
+      colnames(data_shoots),
+      c("plot_id", "tree_measure_id", "shoot_measure_id",
+        "dbh_mm", "height_m", "intact_snag", "decaystage", "period")
+    )
+  })
+})
+
+describe("load_data_vegetation()", {
+  data_vegetation <- load_data_vegetation(path_to_fieldmapdb)
+  it("output columns are correct", {
+    expect_equal(
+      colnames(data_vegetation),
+      c("plot_id", "plottype", "totalplotarea_ha", "forest_reserve",
+        "length_core_area_m", "width_core_area_m", "core_area_ha",
+        "subplot_id", "date_vegetation", "year_main_survey",
+        "total_moss_cover_id", "total_herb_cover_id", "total_shrub_cover_id",
+        "total_tree_cover_id", "total_waterlayer_cover_id",
+        "total_soildisturbance_game_id",
+        "period", "plotarea_ha",
+        "moss_cover_interval", "moss_cover_min", "moss_cover_max",
+        "herb_cover_interval", "herb_cover_min", "herb_cover_max",
+        "shrub_cover_interval", "shrub_cover_min", "shrub_cover_max",
+        "tree_cover_interval", "tree_cover_min", "tree_cover_max",
+        "waterlayer_cover_interval",
+        "waterlayer_cover_min", "waterlayer_cover_max",
+        "soildisturbance_game_cover_interval",
+        "soildisturbance_game_cover_min", "soildisturbance_game_cover_max",
+        "moss_cover_mid", "herb_cover_mid", "shrub_cover_mid", "tree_cover_mid",
+        "waterlayer_cover_mid", "soildisturbance_game_cover_mid")
+    )
+  })
+})
+
+describe("load_plotinfo()", {
+  plotinfo <- load_plotinfo(path_to_fieldmapdb)
+  it("output columns are correct", {
+    expect_equal(
+      colnames(plotinfo),
+      c("plot_id", "plottype", "forest_reserve",
+        "survey_trees", "survey_deadw", "survey_veg", "survey_reg",
+        "game_impact_veg", "game_impact_reg", "data_processed",
+        "period", "survey_number", "year_dendro")
+    )
+  })
+})
+
+path_to_height_models <-
+  system.file("example/height_models", package = "forrescalc")
+
+describe("load_height_models()", {
+  height_models <- load_height_models(path_to_height_models)
+  it("output columns are correct", {
+    expect_equal(
+      colnames(height_models),
+      c("plottype", "period", "species", "model", "P1", "P2", "forest_reserve")
+    )
+  })
+})
