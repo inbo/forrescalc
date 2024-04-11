351dd4ac20fb033bc59be2ae327570dbe5adfaaa
ElsLommelen <els.lommelen@inbo.be>
2024-04-11 14:09:19 +0200

    add unittest to check column names/order of calculate_regeneration function (related to issue #68)

---
+++ tests/testthat/test3_calculate_regeneration_basics.R
@@ -0,0 +1,114 @@
+context("test calculate_regeneration basics")
+
+path_to_fieldmapdb <-
+  system.file("example/database/mdb_bosres.sqlite", package = "forrescalc")
+
+data_regeneration <- load_data_regeneration(path_to_fieldmapdb)
+results_regeneration <- calculate_regeneration(data_regeneration)
+
+describe("regeneration_by_plot", {
+  it("output columns are correct", {
+    expect_equal(
+      colnames(results_regeneration[["regeneration_by_plot"]]),
+      c("plottype", "plot_id", "year", "period", "subplot_id",
+        "number_of_tree_species",
+        "nr_of_tree_species_established", "mean_number_established_ha",
+        "lci_number_established_ha", "uci_number_established_ha",
+        "mean_number_seedlings_ha",
+        "lci_number_seedlings_ha", "uci_number_seedlings_ha",
+        "mean_rubbing_damage_perc_established",
+        "lci_rubbing_damage_perc_established",
+        "uci_rubbing_damage_perc_established",
+        "mean_rubbing_damage_perc_seedlings",
+        "lci_rubbing_damage_perc_seedlings",
+        "uci_rubbing_damage_perc_seedlings",
+        "approx_nr_established_ha", "approx_nr_seedlings_ha",
+        "approx_rubbing_damage_perc_established",
+        "approx_rubbing_damage_perc_seedlings")
+    )
+  })
+})
+
+describe("regeneration_by_plot_height", {
+  it("output columns are correct", {
+    expect_equal(
+      colnames(results_regeneration[["regeneration_by_plot_height"]]),
+      c("plottype", "plot_id", "year", "period", "height_class", "subplot_id",
+        "number_of_tree_species", "rubbing_damage_perc",
+        "approx_nr_regeneration_ha", "mean_number_of_regeneration_ha",
+        "lci_number_of_regeneration_ha", "uci_number_of_regeneration_ha")
+    )
+  })
+})
+
+describe("regeneration_by_plot_species", {
+  it("output columns are correct", {
+    expect_equal(
+      colnames(results_regeneration[["regeneration_by_plot_species"]]),
+      c("plottype", "plot_id", "year", "period", "subplot_id", "species",
+        "mean_number_established_ha",
+        "lci_number_established_ha", "uci_number_established_ha",
+        "mean_number_seedlings_ha",
+        "lci_number_seedlings_ha", "uci_number_seedlings_ha",
+        "mean_rubbing_damage_perc_established",
+        "lci_rubbing_damage_perc_established",
+        "uci_rubbing_damage_perc_established",
+        "mean_rubbing_damage_perc_seedlings",
+        "lci_rubbing_damage_perc_seedlings",
+        "uci_rubbing_damage_perc_seedlings",
+        "approx_nr_established_ha", "approx_nr_seedlings_ha",
+        "approx_rubbing_damage_perc_established",
+        "approx_rubbing_damage_perc_seedlings")
+    )
+  })
+})
+
+describe("regeneration_by_plot_height_species", {
+  it("output columns are correct", {
+    expect_equal(
+      colnames(results_regeneration[["regeneration_by_plot_height_species"]]),
+      c("plottype", "plot_id", "year", "period", "height_class", "species",
+        "subplot_id", "rubbing_damage_perc",
+        "approx_nr_regeneration_ha", "mean_number_of_regeneration_ha",
+        "lci_number_of_regeneration_ha", "uci_number_of_regeneration_ha")
+    )
+  })
+})
+
+describe("regeneration_by_core_area_species", {
+  it("output columns are correct", {
+    expect_equal(
+      colnames(results_regeneration[["regeneration_by_core_area_species"]]),
+      c("plottype", "plot_id", "year", "period", "species",
+        "mean_number_established_ha",
+        "lci_number_established_ha", "uci_number_established_ha",
+        "mean_number_seedlings_ha",
+        "lci_number_seedlings_ha", "uci_number_seedlings_ha",
+        "mean_rubbing_damage_perc_established",
+        "lci_rubbing_damage_perc_established",
+        "uci_rubbing_damage_perc_established",
+        "mean_rubbing_damage_perc_seedlings",
+        "lci_rubbing_damage_perc_seedlings",
+        "uci_rubbing_damage_perc_seedlings",
+        "approx_nr_established_ha", "approx_nr_seedlings_ha",
+        "approx_rubbing_damage_perc_established",
+        "approx_rubbing_damage_perc_seedlings")
+    )
+  })
+})
+
+describe("regeneration_by_core_area_height_species", {
+  it("output columns are correct", {
+    expect_equal(
+      colnames(
+        results_regeneration[["regeneration_by_core_area_height_species"]]
+      ),
+      c("plottype", "plot_id", "year", "period", "height_class", "species",
+        "nr_of_subplots_with_regeneration",
+        "perc_of_subplots_with_regeneration",
+        "rubbing_damage_perc",
+        "approx_nr_regeneration_ha", "mean_number_of_regeneration_ha",
+        "lci_number_of_regeneration_ha", "uci_number_of_regeneration_ha")
+    )
+  })
+})
