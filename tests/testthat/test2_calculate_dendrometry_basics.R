51aee4c2e362d987bcd65ad3c8b791b0a21a8834
ElsLommelen <els.lommelen@inbo.be>
2024-04-15 15:56:19 +0200

    unittest compose_stem_data(): add test for columns if argument extra_variables = TRUE (issue #68, PR #140)

--- tests/testthat/test2_calculate_dendrometry_basics.R
+++ tests/testthat/test2_calculate_dendrometry_basics.R
@@ -88,3 +88,49 @@ describe("deadw_by_decay_plot_species", {
     )
   })
 })
+
+describe("compose_stem_data()", {
+  stem_data <- compose_stem_data(data_dendro, data_shoots)
+  data_dendro_extra <-
+    load_data_dendrometry(path_to_fieldmapdb, extra_variables = TRUE)
+  data_shoots_extra <-
+    load_data_shoots(path_to_fieldmapdb, extra_variables = TRUE)
+  stem_data_extra <-
+    compose_stem_data(
+      data_dendro_extra, data_shoots_extra, extra_variables = TRUE
+    )
+  it("output columns are correct", {
+    expect_equal(
+      colnames(stem_data),
+      c("plot_id", "plottype", "totalplotarea_ha", "tree_measure_id", "old_id",
+        "forest_reserve", "date_dendro", "r_A1", "r_A2", "r_A3", "r_A4",
+        "dbh_min_a3", "dbh_min_a3_dead", "dbh_min_a4", "dbh_min_a4_dead",
+        "dbh_min_core_area", "dbh_min_core_area_dead",
+        "length_core_area_m", "width_core_area_m", "core_area_ha",
+        "dbh_mm", "height_m", "species", "alive_dead", "intact_snag",
+        "decaystage", "calc_height_fm",
+        "crown_volume_reduction", "branch_length_reduction",
+        "ind_sht_cop", "period", "year",
+        "subcircle", "subcirclearea_ha", "plotarea_ha", "shoot_measure_id",
+        "dbh_class_5cm", "basal_area_m2")
+    )
+    expect_equal(
+      colnames(stem_data_extra),
+      c("plot_id", "plottype", "totalplotarea_ha", "tree_measure_id", "old_id",
+        "forest_reserve", "date_dendro", "r_A1", "r_A2", "r_A3", "r_A4",
+        "dbh_min_a3", "dbh_min_a3_dead", "dbh_min_a4", "dbh_min_a4_dead",
+        "dbh_min_core_area", "dbh_min_core_area_dead",
+        "length_core_area_m", "width_core_area_m", "core_area_ha",
+        "dbh_mm", "height_m", "species", "alive_dead", "intact_snag",
+        "decaystage", "calc_height_fm",
+        "crown_volume_reduction", "branch_length_reduction",
+        "ind_sht_cop",
+        "x_local", "y_local", "coppice_id",
+        "iufro_hght", "iufro_vital", "iufro_socia",
+        "remark", "common_remark",
+        "period", "year",
+        "subcircle", "subcirclearea_ha", "plotarea_ha", "shoot_measure_id",
+        "dbh_class_5cm", "basal_area_m2")
+    )
+  })
+})
