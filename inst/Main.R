10b3f1bded477058d32d3b53f0db6d75858791b8
ElsLommelen <els.lommelen@inbo.be>
2024-04-17 18:09:13 +0200

    update main: add metadata (issue #68)

--- inst/Main.R
+++ inst/Main.R
@@ -9,18 +9,34 @@ path_to_height_models <-
 
 # only when q-tables in Fieldmap have changed
 # (and only mention the changed table)
+temp <- tempfile(fileext = ".xlsx")
+dl <- googledrive::drive_download(
+  googledrive::as_id("17M_TfOyjpqLzsFqQ_w1DXitzI7tnULR6"),
+  path = temp, overwrite = TRUE
+)
 from_access_to_git(
   database = path_to_fieldmap,
   tables = c("qAliveDead", "qSpecies", "qHeightClass_regeneration",
              "qnumber_regeneration_classes", "qdecaystage"),
-  repo_path = path_to_git_forresdat
+  repo_path = path_to_git_forresdat,
+  metadata_path = temp
+)
+
+# download .xlsx with metadata of table plotinfo to (path) 'temp'
+temp <- tempfile(fileext = ".xlsx")
+dl <- googledrive::drive_download(
+  googledrive::as_id("1tUNtlwcSnlVXnri235gqnhIHaBNf1Z2W"), path = temp,
+  overwrite = TRUE
 )
 
 plotinfo <- load_plotinfo(database = path_to_fieldmap)
 save_results_git(
   results = list(plotinfo = plotinfo),
-  repo_path = path_to_git_forresdat
+  repo_path = path_to_git_forresdat,
+  metadata_path = temp
 )
+
+# DENDROMETRY
 
 data_dendro <-
   load_data_dendrometry(
@@ -39,10 +55,20 @@ height_model <- load_height_models(path_to_height_models)
 dendro <- calculate_dendrometry(data_dendro, data_deadwood, data_shoots,
                                 height_model, plotinfo)
 
+# download .xlsx with metadata of dendrometry tables to (path) 'temp'
+temp <- tempfile(fileext = ".xlsx")
+dl <- googledrive::drive_download(
+  googledrive::as_id("1M8vTjaPst9LXaDrN1vPVUrsgjaZtAbRi"), path = temp,
+  overwrite = TRUE
+)
+
 save_results_git(
   results = dendro,
-  repo_path = path_to_git_forresdat
+  repo_path = path_to_git_forresdat,
+  metadata_path = temp
 )
+
+# REGENERATION
 
 data_regeneration <-
   load_data_regeneration(
@@ -51,10 +77,20 @@ data_regeneration <-
 
 regeneration <- calculate_regeneration(data_regeneration)
 
+# download .xlsx with metadata of regeneration tables to (path) 'temp'
+temp <- tempfile(fileext = ".xlsx")
+dl <- googledrive::drive_download(
+  googledrive::as_id("17M_TfOyjpqLzsFqQ_w1DXitzI7tnULR6"), path = temp,
+  overwrite = TRUE
+)
+
 save_results_git(
   results = regeneration,
-  repo_path = path_to_git_forresdat
+  repo_path = path_to_git_forresdat,
+  metadata_path = temp
 )
+
+# VEGETATION
 
 data_vegetation <-
   load_data_vegetation(
@@ -67,7 +103,15 @@ data_herblayer <-
 
 vegetation <- calculate_vegetation(data_vegetation, data_herblayer)
 
+# download .xlsx with metadata of vegetation tables to (path) 'temp'
+temp <- tempfile(fileext = ".xlsx")
+dl <- googledrive::drive_download(
+  googledrive::as_id("1Ywwgb0WKGWv9OqJd_rXjZRYDJVpW_KOJ"), path = temp,
+  overwrite = TRUE
+)
+
 save_results_git(
   results = vegetation,
-  repo_path = path_to_git_forresdat
+  repo_path = path_to_git_forresdat,
+  metadata_path = temp
 )
