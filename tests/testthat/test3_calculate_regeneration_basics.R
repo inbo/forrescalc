2bf1005c33479e882c442d6967404fc390de8fe7
Anja Leyman <anja.leyman@inbo.be>
2024-04-19 15:13:06 +0200

    unittests: - test 3  reg: volgorde aangeapst: period voor year; approx naar voren

--- tests/testthat/test3_calculate_regeneration_basics.R
+++ tests/testthat/test3_calculate_regeneration_basics.R
@@ -10,42 +10,11 @@ describe("regeneration_by_plot", {
   it("output columns are correct", {
     expect_equal(
       colnames(results_regeneration[["regeneration_by_plot"]]),
-      c("plottype", "plot_id", "year", "period", "subplot_id",
-        "number_of_tree_species",
-        "nr_of_tree_species_established", "mean_number_established_ha",
-        "lci_number_established_ha", "uci_number_established_ha",
-        "mean_number_seedlings_ha",
-        "lci_number_seedlings_ha", "uci_number_seedlings_ha",
-        "mean_rubbing_damage_perc_established",
-        "lci_rubbing_damage_perc_established",
-        "uci_rubbing_damage_perc_established",
-        "mean_rubbing_damage_perc_seedlings",
-        "lci_rubbing_damage_perc_seedlings",
-        "uci_rubbing_damage_perc_seedlings",
+      c("plottype", "plot_id", "subplot_id", "period", "year",
+        "number_of_tree_species", "nr_of_tree_species_established",
         "approx_nr_established_ha", "approx_nr_seedlings_ha",
         "approx_rubbing_damage_perc_established",
-        "approx_rubbing_damage_perc_seedlings")
-    )
-  })
-})
-
-describe("regeneration_by_plot_height", {
-  it("output columns are correct", {
-    expect_equal(
-      colnames(results_regeneration[["regeneration_by_plot_height"]]),
-      c("plottype", "plot_id", "year", "period", "height_class", "subplot_id",
-        "number_of_tree_species", "rubbing_damage_perc",
-        "approx_nr_regeneration_ha", "mean_number_of_regeneration_ha",
-        "lci_number_of_regeneration_ha", "uci_number_of_regeneration_ha")
-    )
-  })
-})
-
-describe("regeneration_by_plot_species", {
-  it("output columns are correct", {
-    expect_equal(
-      colnames(results_regeneration[["regeneration_by_plot_species"]]),
-      c("plottype", "plot_id", "year", "period", "subplot_id", "species",
+        "approx_rubbing_damage_perc_seedlings",
         "mean_number_established_ha",
         "lci_number_established_ha", "uci_number_established_ha",
         "mean_number_seedlings_ha",
@@ -55,10 +24,42 @@ describe("regeneration_by_plot_species", {
         "uci_rubbing_damage_perc_established",
         "mean_rubbing_damage_perc_seedlings",
         "lci_rubbing_damage_perc_seedlings",
-        "uci_rubbing_damage_perc_seedlings",
+        "uci_rubbing_damage_perc_seedlings")
+    )
+  })
+})
+
+describe("regeneration_by_plot_height", {
+  it("output columns are correct", {
+    expect_equal(
+      colnames(results_regeneration[["regeneration_by_plot_height"]]),
+      c("plottype", "plot_id", "subplot_id", "period", "year",
+        "height_class", "number_of_tree_species",
+        "approx_nr_regeneration_ha", "approx_rubbing_damage_perc",
+        "mean_number_of_regeneration_ha",
+        "lci_number_of_regeneration_ha", "uci_number_of_regeneration_ha")
+    )
+  })
+})
+
+describe("regeneration_by_plot_species", {
+  it("output columns are correct", {
+    expect_equal(
+      colnames(results_regeneration[["regeneration_by_plot_species"]]),
+      c("plottype", "plot_id", "subplot_id", "period", "year", "species",
         "approx_nr_established_ha", "approx_nr_seedlings_ha",
         "approx_rubbing_damage_perc_established",
-        "approx_rubbing_damage_perc_seedlings")
+        "approx_rubbing_damage_perc_seedlings",
+        "mean_number_established_ha",
+        "lci_number_established_ha", "uci_number_established_ha",
+        "mean_number_seedlings_ha",
+        "lci_number_seedlings_ha", "uci_number_seedlings_ha",
+        "mean_rubbing_damage_perc_established",
+        "lci_rubbing_damage_perc_established",
+        "uci_rubbing_damage_perc_established",
+        "mean_rubbing_damage_perc_seedlings",
+        "lci_rubbing_damage_perc_seedlings",
+        "uci_rubbing_damage_perc_seedlings")
     )
   })
 })
@@ -67,8 +68,8 @@ describe("regeneration_by_plot_height_species", {
   it("output columns are correct", {
     expect_equal(
       colnames(results_regeneration[["regeneration_by_plot_height_species"]]),
-      c("plottype", "plot_id", "year", "period", "height_class", "species",
-        "subplot_id", "rubbing_damage_perc",
+      c("plottype", "plot_id", "subplot_id", "period", "year",
+        "height_class", "species", "approx_rubbing_damage_perc",
         "approx_nr_regeneration_ha", "mean_number_of_regeneration_ha",
         "lci_number_of_regeneration_ha", "uci_number_of_regeneration_ha")
     )
@@ -79,7 +80,10 @@ describe("regeneration_by_core_area_species", {
   it("output columns are correct", {
     expect_equal(
       colnames(results_regeneration[["regeneration_by_core_area_species"]]),
-      c("plottype", "plot_id", "year", "period", "species",
+      c("plottype", "plot_id", "period", "year", "species",
+        "approx_nr_established_ha", "approx_nr_seedlings_ha",
+        "approx_rubbing_damage_perc_established",
+        "approx_rubbing_damage_perc_seedlings",
         "mean_number_established_ha",
         "lci_number_established_ha", "uci_number_established_ha",
         "mean_number_seedlings_ha",
@@ -89,10 +93,7 @@ describe("regeneration_by_core_area_species", {
         "uci_rubbing_damage_perc_established",
         "mean_rubbing_damage_perc_seedlings",
         "lci_rubbing_damage_perc_seedlings",
-        "uci_rubbing_damage_perc_seedlings",
-        "approx_nr_established_ha", "approx_nr_seedlings_ha",
-        "approx_rubbing_damage_perc_established",
-        "approx_rubbing_damage_perc_seedlings")
+        "uci_rubbing_damage_perc_seedlings")
     )
   })
 })
@@ -103,7 +104,7 @@ describe("regeneration_by_core_area_height_species", {
       colnames(
         results_regeneration[["regeneration_by_core_area_height_species"]]
       ),
-      c("plottype", "plot_id", "year", "period", "height_class", "species",
+      c("plottype", "plot_id", "period", "year", "height_class", "species",
         "nr_of_subplots_with_regeneration",
         "perc_of_subplots_with_regeneration",
         "rubbing_damage_perc",
