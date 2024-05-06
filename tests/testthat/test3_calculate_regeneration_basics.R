31849002772126e8539a2c13a8e9b1b77488653b
ElsLommelen <els.lommelen@inbo.be>
2024-05-06 15:19:52 +0200

    change order of columns to be in line with other tables

--- tests/testthat/test3_calculate_regeneration_basics.R
+++ tests/testthat/test3_calculate_regeneration_basics.R
@@ -110,8 +110,8 @@ describe("regeneration_by_core_area_height_species", {
       c("plottype", "plot_id", "period", "year", "height_class", "species",
         "nr_of_subplots_with_regeneration",
         "perc_subplots_with_regeneration",
-        "approx_rubbing_damage_perc",
-        "approx_nr_regeneration_ha", "mean_number_of_regeneration_ha",
+        "approx_nr_regeneration_ha", "approx_rubbing_damage_perc",
+        "mean_number_of_regeneration_ha",
         "lci_number_of_regeneration_ha", "uci_number_of_regeneration_ha")
     )
   })
