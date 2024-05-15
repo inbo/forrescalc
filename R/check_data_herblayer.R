2cc12b671cbe07de41e0a148d15c1d408a200054
Anja Leyman <anja.leyman@inbo.be>
2024-05-15 10:25:19 +0200

    check_data_herblayer: browse-index mag NA zijn, beter zou zijn om te checken in cominatie met plodetails (GemeImpactVegObserved)

--- R/check_data_herblayer.R
+++ R/check_data_herblayer.R
@@ -92,14 +92,10 @@ check_data_herblayer <- function(database, forest_reserve = "all") {
       n_records = NULL,
       field_browse_index =
         ifelse(
-          is.na(.data$browse_index) & .data$not_na_browse_index, "missing", NA
-        ),
-      field_browse_index =
-        ifelse(
           !is.na(.data$browse_index) &
             !.data$browse_index %in% data_browseindex$browse_index_id,
           "not in lookuplist",
-          .data$field_browse_index
+          NA
         )
     ) %>%
     pivot_longer(
