f47a944537eefa8e32dcb29ee679db4a50e67276
Anja Leyman <anja.leyman@inbo.be>
2024-06-13 07:32:12 +0200

    Update R/check_data_regspecies.R
    Co-authored-by: Els Lommelen <els.lommelen@inbo.be>

--- R/check_data_regspecies.R
+++ R/check_data_regspecies.R
@@ -105,7 +105,7 @@ check_data_regspecies <- function(database, forest_reserve = "all") {
     mutate(
       field_number_class =
         ifelse(
-          is.na(.data$number_class) & period == 3 &
+          is.na(.data$number_class) & period >= 3 &
             .data$heightclass %in% c(1000, 2000, 5000, 6000),
           "missing", NA
         ),
