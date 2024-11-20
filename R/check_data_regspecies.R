af7e6f62fcbccddf8e76e7b9000f5a5273589a28
Anja Leyman <anja.leyman@inbo.be>
2024-11-20 11:01:49 +0100

    check_data_regspecies: als geen soort genoteerd werd, dan geen zin om aantal te checken

--- R/check_data_regspecies.R
+++ R/check_data_regspecies.R
@@ -105,14 +105,14 @@ check_data_regspecies <- function(database, forest_reserve = "all") {
     mutate(
       field_number_class =
         ifelse(
-          is.na(.data$number_class) & .data$period >= 3 &
-            .data$heightclass %in% c(1000, 2000, 5000, 6000),
+          is.na(.data$number_class) & .data$period >= 3 & !is.na(species) &
+            .data$heightclass %in% c(1000, 2000, 5000),
           "missing", NA
         ),
       field_number =
         ifelse(
-          is.na(.data$number) & .data$period >= 3 &
-            .data$heightclass %in% c(3000, 4000, 7000, 8000),
+          is.na(.data$number) & .data$period >= 3 & !is.na(species) &
+            .data$heightclass %in% c(3000, 4000, 6000, 7000, 8000),
           "missing", NA
         ),
       number_and_numberclass = NA,
